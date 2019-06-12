a3e0266d9c33013e2ffb97627599a92333fa1532
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-06-12 12:27:01 -1000

    difference

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -150,8 +150,15 @@ global.bruhdash = {
 
   // returns an array with specified values excluded
   // [difference(array, [values])]
-  difference: function() {
-
+  difference: function(array, values) {
+    for (i = 0; i < array.length; i++){
+      for (j = 0; j < values.length; j++){
+        if (array[i] === values[j]){
+          array.splice(i, 1);
+        }
+      }
+    }
+    return array;
   },
 
   /*******************
